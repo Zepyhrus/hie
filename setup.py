2c1faff54d973f77b627c3402b54fc4264bf61be
Zepyhrus <sherkfung@gmail.com>
2020-07-17 10:03:06 +0800

    add LICENSE

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+
+setuptools.setup(
+  name="hie",
+  version="0.1.0",
+  author="sherk",
+  author_email="sherkfung@gmail.com",
+  description="A heritance of COCO-api with binary matching",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Zepyhrus/hie",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
